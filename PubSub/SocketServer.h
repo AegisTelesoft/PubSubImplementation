4ba4493a0dd49a7bda5b125a5d417c079c876541
AegisTelesoft <egillk@telesoftas.com>
2017-06-02 13:45:38 +0300

    Added SpdLog logger

--- PubSub/SocketServer.h
+++ PubSub/SocketServer.h
@@ -1,6 +1,9 @@
 #pragma Once
 
-#define BUFFER_SIZE 1024
+#include "spdlog/spdlog.h"
+#include <memory>
+
+#define BUFFER_SIZE 10240
 #define PORT "8082"
 
 #include <winsock2.h>
@@ -19,11 +22,13 @@
 class HandlerData
 {
 public:
-    HandlerData(CancelationToken& token, SOCKET clientSocket);
+    HandlerData(CancelationToken& token, SOCKET clientSocket, struct sockaddr_in clientAddr, std::shared_ptr<spdlog::logger>& logger);
 
 public:
-    CancelationToken& Token;
-    SOCKET            Client;
+    std::shared_ptr<spdlog::logger>& Logger;
+    CancelationToken&                Token;
+    SOCKET                           Client;
+    struct sockaddr_in               ClientAddr;
 };
 
 class SocketServer
@@ -41,6 +46,7 @@ private:
 private:
     WSADATA m_WSAData;
     SOCKET  m_ListenSocket;
+    std::shared_ptr<spdlog::logger> m_fileLogger;
 
     std::vector<std::thread>   m_connectionHandlers;
     CancelationToken           m_token;
